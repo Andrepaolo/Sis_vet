18df69e66020f10f49e5451688474b5476b1bf80
Andrepaolo <andrepaol32@gmail.com>
2024-09-18 23:54:29 -0500

    Spring boot initialize

---
+++ src/main/java/com/veterinaria/sistema/entity/animal.java
@@ -0,0 +1,144 @@
+package com.veterinaria.sistema.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="animales")
+
+public class animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long animalId;
+
+    private String tipo;
+    private String raza;
+    private String nombre;
+
+    @Column(name = "fecha_nacimiento")
+    private LocalDate fechaNacimiento;
+
+    private String sexo;
+
+    @Column(name = "registro_fecha")
+    private LocalDate registroFecha;
+
+    private String observaciones;
+
+    @Column(name = "fecha_creacion")
+    private LocalDate fechaCreacion;
+
+    @Column(name = "usuario_creacion")
+    private Long usuarioCreacion;
+
+    @Column(name = "fecha_modificacion")
+    private LocalDate fechaModificacion;
+
+    @Column(name = "usuario_modificacion")
+    private Long usuarioModificacion;
+
+    // Getters y Setters
+
+    public Long getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(Long animalId) {
+        this.animalId = animalId;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public LocalDate getRegistroFecha() {
+        return registroFecha;
+    }
+
+    public void setRegistroFecha(LocalDate registroFecha) {
+        this.registroFecha = registroFecha;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Long getUsuarioCreacion() {
+        return usuarioCreacion;
+    }
+
+    public void setUsuarioCreacion(Long usuarioCreacion) {
+        this.usuarioCreacion = usuarioCreacion;
+    }
+
+    public LocalDate getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(LocalDate fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public Long getUsuarioModificacion() {
+        return usuarioModificacion;
+    }
+
+    public void setUsuarioModificacion(Long usuarioModificacion) {
+        this.usuarioModificacion = usuarioModificacion;
+    }
+    
+}
---
+++ src/test/java/com/veterinaria/sistema/VeterinariaSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
